4acfeff0e89adee8c8fa14a669bd020671200a8d
chenxiaodong <1346650911@qq.com>
2019-12-26 14:08:31 +0800

    新解法

--- 01-两数之和/add.py
+++ 01-两数之和/add.py
@@ -1,13 +1,10 @@
-def twoSum(nums, target):
-    hashmap = {}
-    for ind, num in enumerate(nums):
-        hashmap[num] = ind
-
-    for i, num in enumerate(nums):
-        j = hashmap.get(target - num)
-        if j is not None and i != j:
-            return [i, j]
-
+def two_sum(nums, target):
+    """这样写更直观，遍历列表同时查字典"""
+    dct = {}
+    for i, n in enumerate(nums):
+        if target - n in dct:
+            return [dct[target - n], i]
+        dct[n] = i
 
 if __name__ == '__main__':
     num = [3, 3]
